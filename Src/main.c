f3946ccce908f263e05287c13aae557236650033
Lins Paulson <linspaulson29@gmail.com>
2025-11-20 19:44:13 +0530

    feature: Added blinking function

--- Src/main.c
+++ Src/main.c
@@ -1,7 +1,7 @@
 // PA5 - LED
 // 0x4800 0000 - GPIOA
 // 0x4002 1000 - RCC
-
+#include <stdint.h>
 #define RCC_BASE_ADDR 0x40021000U
 #define GPIO_BASE_ADDR 0x48000000U
 
@@ -14,6 +14,12 @@
 #define GPIOA_ODR_OFFSET  0x14U
 #define GPIOA_ODR_REG     (*(unsigned int volatile *)(GPIO_BASE_ADDR + GPIOA_ODR_OFFSET))
 
+void delay(volatile uint32_t d)
+{
+	while(d--);
+}
+
+
 int main(void)
 {
 	RCC_AHBENR_REG |= (1U << 17);
@@ -21,8 +27,14 @@ int main(void)
 	GPIOA_MODER_REG |= (1U << 10);
 	GPIOA_MODER_REG &= ~(1U << 11);
 
-	GPIOA_ODR_REG |= (1U << 5);
 
-	while(1);
+	while(1)
+	{
+	GPIOA_ODR_REG |= (1U << 5);
+	delay(500000);
+
+	GPIOA_ODR_REG &= ~(1U << 5);
+	delay(500000);
+	}
 	return 0;
 }
